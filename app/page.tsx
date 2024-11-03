e01bd090ca8f20f1e612b141d0d1c8b7b1e167f8
YEAHx4 <developerstarlight@gmail.com>
2024-11-03 23:21:37 +0900

    Greeting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 import Typing from "@/components/animations/typing";
+import cn from "@yeahx4/cn";
 
 export default function Home() {
   return (
-    <div>
-      <Typing value={["Hello World!", "I'm Kyusan Kim"]} />
+    <div className="flex justify-center mt-32 px-4">
+      <div
+        className={cn(
+          "max-w-5xl w-full flex justify-between md:flex-row",
+          "flex-col-reverse"
+        )}
+      >
+        <div>
+          <Typing
+            value={["Hello World!", "I'm Kyusan Kim"]}
+            loop
+            className={cn("font-bold text-4xl md:text-6xl opacity-65")}
+          />
+          <div className="flex flex-col mt-16 text-lg opacity-95 gap-1">
+            <span>안녕하세요!</span>
+            <span>대학생 개발자 김규산입니다.</span>
+            <span>
+              소통을 통해 <strong>나</strong>부터 만족할 수 있는 소프트웨어를
+              만듭니다.
+            </span>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/yeahx4.png"
+            alt="김규산"
+            className="w-40 h-40 md:w-60 md:h-60 rounded-full"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- components/animations/typing.tsx
+++ components/animations/typing.tsx
@@ -2,6 +2,32 @@
 
 import { ReactTyped } from "react-typed";
 
-export default function Typing({ value }: { value: string[] }) {
-  return <ReactTyped strings={value} />;
+export default function Typing({
+  value,
+  loop,
+  typingSpeed = 50,
+  backSpeed = 50,
+  smartBackspace,
+  startWhenVisible,
+  className,
+}: {
+  value: string[];
+  loop?: boolean;
+  typingSpeed?: number;
+  backSpeed?: number;
+  smartBackspace?: boolean;
+  startWhenVisible?: boolean;
+  className?: string;
+}) {
+  return (
+    <ReactTyped
+      strings={value}
+      loop={loop}
+      typeSpeed={typingSpeed}
+      backSpeed={backSpeed}
+      smartBackspace={smartBackspace}
+      startWhenVisible={startWhenVisible}
+      className={className}
+    />
+  );
 }
